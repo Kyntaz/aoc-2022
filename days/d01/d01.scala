99375d30e282f1ec287a3498b07102683d3bbada
Kyntaz <1260pedroquintas@gmail.com>
2022-12-01 21:07:35 +0000

    Make day 01 prettier

--- days/d01/d01.scala
+++ days/d01/d01.scala
@@ -1,17 +1,25 @@
 import AoC.utils.blockBy;
 
 def getCaloryLists = {
-    io.Source.stdin.getLines.toList.blockBy(_ == "")
-        .map(_.map(_.toInt));
-
+    io.Source.stdin.getLines.toList
+        .blockBy(_ == "")
+        .map {
+            _.map(_.toInt);
+        }
 }
 
 def p1() = {
-    getCaloryLists.map(_.sum).max;
+    getCaloryLists
+        .map(_.sum)
+        .max;
 }
 
 def p2() = {
-    getCaloryLists.map(_.sum).sortWith(_ > _).slice(0, 3).sum;
+    getCaloryLists
+        .map(_.sum)
+        .sortWith(_ > _)
+        .slice(0, 3)
+        .sum;
 }
 
 @main def main(part: Int) = {
