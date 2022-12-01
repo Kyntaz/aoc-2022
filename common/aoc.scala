577a85b2a0b4100cf151dd65af3da3262de03348
Kyntaz <1260pedroquintas@gmail.com>
2022-12-01 22:11:28 +0000

    Improve common code

--- common/aoc.scala
+++ common/aoc.scala
@@ -1,11 +1,24 @@
 package AoC;
 
+import collection.mutable.ListBuffer;
+
 private var PART = 0;
 
+/**
+  * Declares that you are starting to solve an AoC problem.
+  *
+  * @param part the part you are attempting to solve.
+  */
 def start(part: Int) = {
     PART = part;
 }
 
+/**
+  * Declares the solution to a given part of the AoC problem.
+  *
+  * @param part the part to which the solution applies.
+  * @param solution the solution to the given part.
+  */
 def solve(part: Int, solution: () => Any) = {
     if (part == PART) {
         println(s"P$part: ${solution()}");
@@ -13,19 +26,26 @@ def solve(part: Int, solution: () => Any) = {
 }
 
 package utils {
-    extension [T](l: List[T]) def blockBy(f: T => Boolean) = {
-        var blocks = List[List[T]]();
-        var block = List[T]();
+    /**
+      * Splits a list into a list of lists, using elements that satisfy the `predicate` as separators.
+      * The separators are excluded from the result.
+      *
+      * @param predicate the predicate that decides whether an element is a separator or not.
+      * @return the list of lists.
+      */
+    extension [T](l: List[T]) def blockBy(predicate: T => Boolean) = {
+        val blocks = ListBuffer[List[T]]();
+        var block = ListBuffer[T]();
 
         for (el <- l) {
-            if (f(el)) {
-                blocks = blocks.appended(block);
-                block = List[T]();
+            if (predicate(el)) {
+                blocks += block.toList;
+                block = ListBuffer[T]();
             } else {
-                block = block.appended(el);
+                block += el;
             }
         }
 
-        blocks;
+        blocks.toList;
     }
 }
