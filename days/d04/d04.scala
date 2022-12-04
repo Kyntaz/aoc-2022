41932e172f1c92979b270aa320c4014a1512b19d
Kyntaz <1260pedroquintas@gmail.com>
2022-12-04 17:45:04 +0000

    Day 4

--- days/d04/d04.scala
+++ days/d04/d04.scala
@@ -1,5 +1,42 @@
+def readSections = {
+    io.Source.stdin.getLines()
+        .map { line =>
+            val Array(elf1, elf2) = line.split(",");
+            val Array(start1, end1) = elf1.split("-");
+            val Array(start2, end2) = elf2.split("-");
+            ((start1.toInt, end1.toInt), (start2.toInt, end2.toInt));
+        }
+}
+
+def fullyOverlap(sections: ((Int, Int), (Int, Int))) = {
+    val ((s1, e1), (s2, e2)) = sections;
+    if (
+        (s1 >= s2 && e1 <= e2) ||
+        (s2 >= s1 && e2 <= e1)
+    ) true else false;
+}
+
+def overlap(sections: ((Int, Int), (Int, Int))) = {
+    val ((s1, e1), (s2, e2)) = sections;
+    if (
+        (s1 > e2) || (s2 > e1)
+    ) false else true;
+}
+
+def p1() = {
+    readSections
+        .filter(fullyOverlap)
+        .length;
+}
+
+def p2() = {
+    readSections
+        .filter(overlap)
+        .length;
+}
+
 @main def main(part: Int) = {
     AoC.start(part);
-    AoC.solve(1, () => "TODO");
-    AoC.solve(2, () => "TODO");
+    AoC.solve(1, p1);
+    AoC.solve(2, p2);
 }
