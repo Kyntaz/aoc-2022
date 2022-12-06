43635af998ef4fb3efea2cf0a40d9fac48bb4afc
Kyntaz <1260pedroquintas@gmail.com>
2022-12-06 18:18:31 +0000

    Day 6

--- days/d06/d06.scala
+++ days/d06/d06.scala
@@ -1,5 +1,20 @@
+def getSignal = {
+    io.StdIn.readLine();
+}
+
+def p(windowSize: Int) = () => {
+    val (_, i) = getSignal.toList
+        .sliding(windowSize)
+        .zipWithIndex
+        .filter { iWindow =>
+            val (window, _) = iWindow;
+            window.length == window.toSet.size;
+        }.toList.head;
+    i + windowSize;
+}
+
 @main def main(part: Int) = {
     AoC.start(part);
-    AoC.solve(1, () => "TODO");
-    AoC.solve(2, () => "TODO");
+    AoC.solve(1, p(4));
+    AoC.solve(2, p(14));
 }
