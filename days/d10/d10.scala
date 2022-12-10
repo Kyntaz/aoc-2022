3fdf9c7ff03ab3d7fd877c63f7a1e5ebf1e74a69
Kyntaz <1260pedroquintas@gmail.com>
2022-12-10 12:55:27 +0000

    Day 10

--- days/d10/d10.scala
+++ days/d10/d10.scala
@@ -1,5 +1,62 @@
+import scala.collection.mutable.ListBuffer;
+
+def getInstructions = {
+    io.Source.stdin.getLines()
+        .map { line => 
+            val args = line.split(" ");
+            args match {
+                case Array("addx", arg) => ("addx", arg.toInt);
+                case Array("noop") => ("noop", 0);
+            }
+        }.toList
+}
+
+def makeFx(instructions: List[(String, Int)]) = {
+    val fx = ListBuffer[Int]();
+    var x = 1;
+
+    for (inst <- instructions) {
+        inst match {
+            case ("addx", arg) => {
+                fx += x;
+                fx += x;
+                x += arg;
+            }
+            case ("noop", _) => {
+                fx += x;
+            }
+        }
+    }
+
+    fx.toList;
+}
+
+def makeFcrt(fx: List[Int]) = {
+    fx.zipWithIndex
+        .map { pixel =>
+            val (x, pos) = pixel;
+            val col = pos % 40;
+            if (Math.abs(col - x) <= 1) '#' else ' ';
+        }
+}
+
+def p1() = {
+    val fx = makeFx(getInstructions);
+    List(19, 59, 99, 139, 179, 219)
+        .map(c => fx(c) * (c + 1))
+        .sum;
+}
+
+def p2() = {
+    val fx = makeFx(getInstructions);
+    makeFcrt(fx)
+        .grouped(40)
+        .map(_.mkString)
+        .foreach(println(_));
+}
+
 @main def main(part: Int) = {
     AoC.start(part);
-    AoC.solve(1, () => "TODO");
-    AoC.solve(2, () => "TODO");
+    AoC.solve(1, p1);
+    AoC.solve(2, p2);
 }
